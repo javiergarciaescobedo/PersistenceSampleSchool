8f90b0c34a03c351168b8b9a71bf87d8f835831e
Javier García Escobedo <correo@javiergarciaescobedo.es>
2015-04-27 22:15:02 +0200

    Initial commit

---
+++ src/es/javiergarciaescobedo/persistencesampleschool/entity/Student.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.javiergarciaescobedo.persistencesampleschool.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author javier
+ */
+@Entity
+@Table(name = "STUDENT")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Student.findAll", query = "SELECT s FROM Student s"),
+    @NamedQuery(name = "Student.findById", query = "SELECT s FROM Student s WHERE s.id = :id"),
+    @NamedQuery(name = "Student.findByName", query = "SELECT s FROM Student s WHERE s.name = :name"),
+    @NamedQuery(name = "Student.findBySurnames", query = "SELECT s FROM Student s WHERE s.surnames = :surnames"),
+    @NamedQuery(name = "Student.findByDateBirth", query = "SELECT s FROM Student s WHERE s.dateBirth = :dateBirth"),
+    @NamedQuery(name = "Student.findByGrade", query = "SELECT s FROM Student s WHERE s.grade = :grade")})
+public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "NAME")
+    private String name;
+    @Basic(optional = false)
+    @Column(name = "SURNAMES")
+    private String surnames;
+    @Column(name = "DATE_BIRTH")
+    @Temporal(TemporalType.DATE)
+    private Date dateBirth;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "GRADE")
+    private BigDecimal grade;
+    @JoinColumn(name = "CLASS_GROUP", referencedColumnName = "ID")
+    @ManyToOne
+    private ClassGroup classGroup;
+
+    public Student() {
+    }
+
+    public Student(Integer id) {
+        this.id = id;
+    }
+
+    public Student(Integer id, String name, String surnames) {
+        this.id = id;
+        this.name = name;
+        this.surnames = surnames;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurnames() {
+        return surnames;
+    }
+
+    public void setSurnames(String surnames) {
+        this.surnames = surnames;
+    }
+
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(Date dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public BigDecimal getGrade() {
+        return grade;
+    }
+
+    public void setGrade(BigDecimal grade) {
+        this.grade = grade;
+    }
+
+    public ClassGroup getClassGroup() {
+        return classGroup;
+    }
+
+    public void setClassGroup(ClassGroup classGroup) {
+        this.classGroup = classGroup;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Student)) {
+            return false;
+        }
+        Student other = (Student) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "es.javiergarciaescobedo.persistencesampleschool.entity.Student[ id=" + id + " ]";
+    }
+    
+}
